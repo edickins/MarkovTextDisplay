6547184c903c1a1cc20adf26769e8efada286603
ecdickins <ed.dickins@bleepbloop.net>
2024-07-31 14:14:15 +0100

    added client from vite_react_eslint_prettier_ts_tailwind boilerplate project

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-no-useless-fragment': 0,
+    'react/react-in-jsx-scope': 0,
+    'jsx-quotes': ['error', 'prefer-single'],
+    '@typescript-eslint/comma-dangle': ['error', 'never'],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+  ignorePatterns: tsConfig.exclude,
+};
---
+++ client/.prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  trailingCommas: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+};
